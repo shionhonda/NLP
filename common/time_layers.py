0e49548de55577edcfd39ae0302e330cd0715028
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2018-11-06 20:33:58 +0900

    Finish Chap6

--- common/time_layers.py
+++ common/time_layers.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-from common.np import *
+from common.np import *  # import numpy as np (or import cupy as np)
 from common.layers import *
 from common.functions import sigmoid
+
 
 class RNN:
     def __init__(self, Wx, Wh, b):
@@ -13,6 +14,7 @@ class RNN:
         Wx, Wh, b = self.params
         t = np.dot(h_prev, Wh) + np.dot(x, Wx) + b
         h_next = np.tanh(t)
+
         self.cache = (x, h_prev, h_next)
         return h_next
 
@@ -20,7 +22,7 @@ class RNN:
         Wx, Wh, b = self.params
         x, h_prev, h_next = self.cache
 
-        dt = dh_next * (1-h_next**2)
+        dt = dh_next * (1 - h_next ** 2)
         db = np.sum(dt, axis=0)
         dWh = np.dot(h_prev.T, dt)
         dh_prev = np.dot(dt, Wh.T)
@@ -33,30 +35,31 @@ class RNN:
 
         return dx, dh_prev
 
+
 class TimeRNN:
     def __init__(self, Wx, Wh, b, stateful=False):
         self.params = [Wx, Wh, b]
         self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
         self.layers = None
 
-        self.h, self.dh, = None, None
+        self.h, self.dh = None, None
         self.stateful = stateful
 
     def forward(self, xs):
-        Wx, Wh, b  = self.params
+        Wx, Wh, b = self.params
         N, T, D = xs.shape
         D, H = Wx.shape
 
-        self.layers =[]
+        self.layers = []
         hs = np.empty((N, T, H), dtype='f')
 
         if not self.stateful or self.h is None:
             self.h = np.zeros((N, H), dtype='f')
 
         for t in range(T):
-            layer = RNN(*self.params) # Unpack
-            self.h = layer.forward(xs[:,t,:], self.h)
-            hs[:,t,:] = self.h
+            layer = RNN(*self.params)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
             self.layers.append(layer)
 
         return hs
@@ -66,13 +69,13 @@ class TimeRNN:
         N, T, H = dhs.shape
         D, H = Wx.shape
 
-        dxs = np.empty((N,T,D), dtype='f')
+        dxs = np.empty((N, T, D), dtype='f')
         dh = 0
-        grads = [0,0,0]
+        grads = [0, 0, 0]
         for t in reversed(range(T)):
             layer = self.layers[t]
-            dx, dh = layer.backward(dhs[:,t,:]+dh)
-            dxs[:,t,:] = dx
+            dx, dh = layer.backward(dhs[:, t, :] + dh)
+            dxs[:, t, :] = dx
 
             for i, grad in enumerate(layer.grads):
                 grads[i] += grad
@@ -89,32 +92,77 @@ class TimeRNN:
     def reset_state(self):
         self.h = None
 
+
 class LSTM:
     def __init__(self, Wx, Wh, b):
+        '''
+        Parameters
+        ----------
+        Wx: 入力`x`用の重みパラーメタ（4つ分の重みをまとめる）
+        Wh: 隠れ状態`h`用の重みパラメータ（4つ分の重みをまとめる）
+        b: バイアス（4つ分のバイアスをまとめる）
+        '''
         self.params = [Wx, Wh, b]
         self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
-        self.layers = None
+        self.cache = None
 
     def forward(self, x, h_prev, c_prev):
         Wx, Wh, b = self.params
         N, H = h_prev.shape
+
         A = np.dot(x, Wx) + np.dot(h_prev, Wh) + b
 
-        f = A[:,:H]
-        g = A[:,H:2*H]
-        i = A[:,2*H:3*H]
-        o = A[:,3*H:]
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
 
         f = sigmoid(f)
         g = np.tanh(g)
         i = sigmoid(i)
         o = sigmoid(o)
 
-        c_next = f*c_prev + g*i
-        h_next = o*np.tanh(c_next)
+        c_next = f * c_prev + g * i
+        h_next = o * np.tanh(c_next)
 
-        self.cache = (x, h_prev, c_prev, i, f, g ,o, c_next)
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
         return h_next, c_next
+
+    def backward(self, dh_next, dc_next):
+        Wx, Wh, b = self.params
+        x, h_prev, c_prev, i, f, g, o, c_next = self.cache
+
+        tanh_c_next = np.tanh(c_next)
+
+        ds = dc_next + (dh_next * o) * (1 - tanh_c_next ** 2)
+
+        dc_prev = ds * f
+
+        di = ds * g
+        df = ds * c_prev
+        do = dh_next * tanh_c_next
+        dg = ds * i
+
+        di *= i * (1 - i)
+        df *= f * (1 - f)
+        do *= o * (1 - o)
+        dg *= (1 - g ** 2)
+
+        dA = np.hstack((df, dg, di, do))
+
+        dWh = np.dot(h_prev.T, dA)
+        dWx = np.dot(x.T, dA)
+        db = dA.sum(axis=0)
+
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+
+        dx = np.dot(dA, Wx.T)
+        dh_prev = np.dot(dA, Wh.T)
+
+        return dx, dh_prev, dc_prev
+
 
 class TimeLSTM:
     def __init__(self, Wx, Wh, b, stateful=False):
@@ -122,23 +170,27 @@ class TimeLSTM:
         self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
         self.layers = None
 
+        self.h, self.c = None, None
+        self.dh = None
+        self.stateful = stateful
+
     def forward(self, xs):
         Wx, Wh, b = self.params
         N, T, D = xs.shape
         H = Wh.shape[0]
 
         self.layers = []
-        hs = np.empty((N,T,H), dtype='f')
+        hs = np.empty((N, T, H), dtype='f')
 
         if not self.stateful or self.h is None:
-            self.h = np.zeros((N,H), dtype='f')
+            self.h = np.zeros((N, H), dtype='f')
         if not self.stateful or self.c is None:
-            self.c = np.zeros((N,H), dtype='f')
+            self.c = np.zeros((N, H), dtype='f')
 
         for t in range(T):
-            layer = LSTM(*self.params) # Unpack
-            self.h, self.c = layer.forward(xs[:,t,:], self.h, self.c)
-            hs[:,t,:] = self.h
+            layer = LSTM(*self.params)
+            self.h, self.c = layer.forward(xs[:, t, :], self.h, self.c)
+            hs[:, t, :] = self.h
 
             self.layers.append(layer)
 
@@ -149,14 +201,14 @@ class TimeLSTM:
         N, T, H = dhs.shape
         D = Wx.shape[0]
 
-        dxs = np.empty((N,T,D), dtype='f')
+        dxs = np.empty((N, T, D), dtype='f')
         dh, dc = 0, 0
 
-        grads = [0,0,0]
+        grads = [0, 0, 0]
         for t in reversed(range(T)):
             layer = self.layers[t]
-            dx, dh, dc = layer.backward(dhs[:,t,:] + dh, dc)
-            dxs[:,t,:] = dx
+            dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)
+            dxs[:, t, :] = dx
             for i, grad in enumerate(layer.grads):
                 grads[i] += grad
 
@@ -165,13 +217,12 @@ class TimeLSTM:
         self.dh = dh
         return dxs
 
-    def set_state(self, h, c):
-        self.h = h
-        self.c = c
+    def set_state(self, h, c=None):
+        self.h, self.c = h, c
 
     def reset_state(self):
-        self.h = None
-        self.c = None
+        self.h, self.c = None, None
+
 
 class TimeEmbedding:
     def __init__(self, W):
@@ -281,3 +332,24 @@ class TimeSoftmaxWithLoss:
         dx = dx.reshape((N, T, V))
 
         return dx
+
+
+class TimeDropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.params, self.grads = [], []
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        self.train_flg = True
+
+    def forward(self, xs):
+        if self.train_flg:
+            flg = np.random.rand(*xs.shape) > self.dropout_ratio
+            scale = 1 / (1.0 - self.dropout_ratio)
+            self.mask = flg.astype(np.float32) * scale
+
+            return xs * self.mask
+        else:
+            return xs
+
+    def backward(self, dout):
+        return dout * self.mask
